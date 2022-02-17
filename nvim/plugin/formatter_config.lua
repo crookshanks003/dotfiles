186e19fe24036382e228256609e8766f54efcd3d
Pritesh <priteshpmantri@gmail.com>
2022-02-17 16:42:09 +0530

    🚀

--- nvim/plugin/formatter_config.lua
+++ nvim/plugin/formatter_config.lua
@@ -1,7 +1,28 @@
-require('formatter').setup({
-	logging = false,
-	filetype = {
-		javascript = {
+local filetypes = {
+	rust = {
+		function()
+			return {
+				exe = "rustfmt",
+				args = {"--emit=stdout"},
+				stdin = true
+			}
+		end
+	},
+	go = {
+		function()
+			return {
+				exe = "gofmt "..vim.api.nvim_buf_get_name(0),
+				stdin = true
+			}
+		end
+	},
+};
+
+local js = {"javascript", "javascriptreact", "typescript", "typescriptreact", "json", "css", "html"}
+local c = {"cpp", "c"}
+
+for _, type in pairs(js) do
+	filetypes[type] =  {
 			function()
 				return {
 					exe = "prettier",
@@ -15,128 +36,26 @@ require('formatter').setup({
 					stdin = true
 				}
 			end
-		},
-		javascriptreact = {
-			function()
-				return {
-					exe = "prettier",
-					args = {
-						"--stdin-filepath", vim.api.nvim_buf_get_name(0),
-						'--single-quote=false',
-						'--tab-width', 4,
-						'--use-tabs',
-						'--print-width', 100,
-					},
-					stdin = true
-				}
-			end
-		},
-		typescript = {
-			function()
-				return {
-					exe = "prettier",
-					args = {
-						"--stdin-filepath", vim.api.nvim_buf_get_name(0),
-						'--single-quote=false',
-						'--tab-width', 4,
-						'--use-tabs',
-						'--print-width', 100,
-					},
-					stdin = true
-				}
-			end
-		},
-		typescriptreact = {
-			function()
-				return {
-					exe = "prettier",
-					args = {
-						"--stdin-filepath", vim.api.nvim_buf_get_name(0),
-						'--single-quote=false',
-						'--tab-width', 4,
-						'--use-tabs',
-						'--print-width', 100,
-						'--jsx-bracket-same-line',
-					},
-					stdin = true
-				}
-			end
-		},
-		json = {
-			function ()
-				return {
-					exe="prettier",
-					args = {
-						"--stdin-filepath", vim.api.nvim_buf_get_name(0),
-						'--single-quote=false',
-						'--tab-width', 4,
-						'--use-tabs',
-						'--print-width', 100,
-					},
-					stdin=true
-				}
-			end
-		},
-		css = {
-			function ()
-				return {
-					exe="prettier",
-					args = {
-						"--stdin-filepath", vim.api.nvim_buf_get_name(0),
-						'--single-quote=false',
-						'--tab-width', 4,
-						'--use-tabs',
-						'--print-width', 100,
-					},
-					stdin=true
-				}
-			end
-		},
-		html = {
-			function()
-				return {
-					exe = "prettier",
-					args = {
-						"--stdin-filepath", vim.api.nvim_buf_get_name(0),
-						'--single-quote=false',
-						'--tab-width', 4,
-						'--use-tabs',
-						'--print-width', 100,
-						'--jsx-bracket-same-line',
-					},
-					stdin = true
-				}
-			end
-		},
-		cpp = {
-			function()
-				return {
-					exe = "clang-format",
-					args = {
-						"--assume-filename", vim.api.nvim_buf_get_name(0),
-						'-style="{ BasedOnStyle: LLVM, UseTab: Always, IndentWidth: 4, TabWidth: 4, AllowShortIfStatementsOnASingleLine: true}"',
-					},
-					stdin = true,
-					cwd = vim.fn.expand('%:p:h')  -- Run clang-format in cwd of the file.
-				}
-			end
-		},
-		rust = {
-			function()
-				return {
-					exe = "rustfmt",
-					args = {"--emit=stdout"},
-					stdin = true
-				}
-			end
-		},
-		go = {
-			function()
-				return {
-					exe = "gofmt "..vim.api.nvim_buf_get_name(0),
-					stdin = true
-				}
-			end
-		},
 	}
+end
+
+for _,type in pairs(c) do
+	filetypes[type] = {
+		function()
+			return {
+				exe = "clang-format",
+				args = {
+					"--assume-filename", vim.api.nvim_buf_get_name(0),
+					'-style="{ BasedOnStyle: LLVM, UseTab: Always, IndentWidth: 4, TabWidth: 4, AllowShortIfStatementsOnASingleLine: true}"',
+				},
+				stdin = true,
+				cwd = vim.fn.expand('%:p:h')  -- Run clang-format in cwd of the file.
+			}
+		end
+	}
+end
+
+require('formatter').setup({
+	logging = false,
+	filetype = filetypes,
 })
